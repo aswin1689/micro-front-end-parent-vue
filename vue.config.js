953986f3c22c64ff7a9f7c3095bb1fe13f4c433c
aswin kumar alapati <aswinalapati1@gmail.com>
2020-07-30 13:27:23 -0400

    add vue config and transport mode

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,7 @@ module.exports = {
     configureWebpack: {
         devServer: {
             transportMode: "ws",
-            injectClient: false,
-            proxy: {
-                '/': {
-                    target: 'http://localhost:5000',
-                    ws: true,
-                    changeOrigin: true,
-                },
-            }
+            injectClient: false
         }
     }
 } 
